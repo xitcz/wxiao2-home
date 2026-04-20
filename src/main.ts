66df14814b3f2dd786cf5478ade67bccf29b27ce
wxiao2 <42410976+xitcz@users.noreply.github.com>
2026-04-21 00:21:48 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -106,15 +106,6 @@ if (!import.meta.env.VITE_CONFIG_TURN || import.meta.env.VITE_CONFIG_TURN != "tr
       })
     });
 } else {
-  /* 原本这里的逻辑是..如果作者信息被修改，则停止项目运行。原本是想拦没有代码更改但删除作者信息直接倒卖的人，因为但凡是个会一点代码的人不可能不会解决这点小问题。这在技术上几乎没有一点防护效果。
-  但是看到确实有用户修改后，思考了一阵，这好像和 MIT 开源文化有冲突...且 MIT 本身也就允许倒卖这种行为叭..（
-  最后..还是决定将这里的完全不运行改成显示提示后继续正常工作，更合理些叭...理解万岁！
-  另外，也请求各位不要随意移除原作者信息，谢谢！ */
-  if (config.author != 'imsyy' || config.efua != 'NanoRocky') {
-    console.warn(`Warning: Somethings error ... , The original author information for this project has been modified. If this was not done by you, please delete the file and download the project code package again. If this was done by you, please do not modify or remove the original author information. Thank you! Of course, you can also choose to ignore this message.`);
-    console.log('Original repository link: https://github.com/NanoRocky/home/blob/EFU/');
-    mountApp();
-  } else {
     mountApp();
   };
 };
